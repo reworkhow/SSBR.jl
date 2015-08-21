1f57c84d74357d72a9c33aee60900973324477af
Hao Cheng <reworkhow@gmail.com>
2015-08-21 00:33:21 -0500

    Update SSBR.jl

--- src/SSBR.jl
+++ src/SSBR.jl
@@ -4,4 +4,14 @@ include("ssFunctions.jl")
 include("ssGibbs.jl")
 include("ssMME.jl")
 
+export calc_Ai
+export read_genotypes
+export make_MMats
+export make_yVecs
+export make_JVecs
+export make_ZMats
+export make_XWMats
+export ssGibbs
+export ssMME
+
 end # module
